be6fe6a47b678dfbddaef3c562797066008e3ea6
Joseph Procter <josephprocter@MacBook-Pro.local>
2026-02-01 22:53:55 +0000

    Add apple touch icon link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -28,8 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+  <head>
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+  </head>
+  <body
+   className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
